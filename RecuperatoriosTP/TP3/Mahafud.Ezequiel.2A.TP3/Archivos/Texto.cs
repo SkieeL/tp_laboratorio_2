041e9821ceb48dab16d37b7e99ea0d2f70034068
SkieeL <Skiel_Mafu-cs@hotmail.com>
2017-11-22 16:35:46 -0300

    Recuperatorios de TP
    Se suben el recuperatorio del TP3.

---
+++ RecuperatoriosTP/TP3/Mahafud.Ezequiel.2A.TP3/Archivos/Texto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        /// <summary>
+        /// Guarda información en un archivo de texto.
+        /// </summary>
+        /// <param name="archivo">Nombre del archivo donde se guardarán los datos.</param>
+        /// <param name="datos">Información a guardar.</param>
+        /// <returns></returns>
+        public bool guardar(string archivo, string datos) {
+            StreamWriter escritura = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + archivo, false, Encoding.UTF8);
+
+            escritura.WriteLine(datos);
+            escritura.Close();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Lee información de un archivo de texto.
+        /// </summary>
+        /// <param name="archivo">Nombre del archivo a leer.</param>
+        /// <param name="datos">Variable de salida donde se almacenará la información leída.</param>
+        /// <returns></returns>
+        public bool leer(string archivo, out string datos) {
+            StreamReader lectura = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + archivo, Encoding.UTF8);
+
+            datos = lectura.ReadToEnd();
+            lectura.Close();
+
+            return true;
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP3/Mahafud.Ezequiel.2A.TP3/TestsUnitarios/UnitTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+
+namespace TestsUnitarios
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void validaNacionalidadInvalidaException() {
+            try {
+                Alumno alumno = new Alumno(1, "Ezequiel", "Mahafud", "90000001", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            }
+            catch (Exception e) {
+                Assert.IsInstanceOfType(e, typeof(NacionalidadInvalidaException));
+            }
+        }
+
+        [TestMethod]
+        public void validaDniInvalidoException() {
+            try {
+                Alumno alumno = new Alumno(1, "Ezequiel", "Mahafud", "-38999220", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            }
+            catch (Exception e) {
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+
+            try {
+                Alumno alumno = new Alumno(1, "Ezequiel", "Mahafud", "38999220", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+                alumno.DNI = -1;
+            }
+            catch (Exception e) {
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+
+            try {
+                Alumno alumno = new Alumno(1, "Ezequiel", "Mahafud", "38999220", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+                alumno.StringToDNI = "DNI";
+            }
+            catch (Exception e) {
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+        }
+
+        [TestMethod]
+        public void validaValorNumerico() {
+            Alumno alumno = new Alumno(1, "Ezequiel", "Mahafud", "38999220", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+
+            Assert.IsInstanceOfType(alumno.DNI, typeof(int));
+        }
+
+        [TestMethod]
+        public void validaValoresNulos() {
+            Alumno alumno = new Alumno(1, "Ezequiel", "Mahafud", "38999220", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+
+            Assert.IsNotNull(alumno.Nombre);
+            Assert.IsNotNull(alumno.Apellido);
+            Assert.IsNotNull(alumno.DNI);
+            Assert.IsNotNull(alumno.Nacionalidad);
+        }
+    }
+}
